4c07ce5ff69a146da589ad31594089bee61d0800
mistarson <yeowuli2@naver.com>
2023-11-21 16:10:24 +0900

    Initial commit

---
+++ src/main/java/F12/newsfeedproject/NewsFeedProjectApplication.java
@@ -0,0 +1,13 @@
+package F12.newsfeedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFeedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsFeedProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/F12/newsfeedproject/NewsFeedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package F12.newsfeedproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFeedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
