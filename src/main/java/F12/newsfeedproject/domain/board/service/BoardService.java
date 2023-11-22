4521692eb0eec166b4e8847e537e5f87c86ecfc4
oneseel <seno1006@gmail.com>
2023-11-22 17:35:04 +0900

    feature: BOARD NOT FOUND EXCEPTION

--- src/main/java/F12/newsfeedproject/domain/board/service/BoardService.java
+++ src/main/java/F12/newsfeedproject/domain/board/service/BoardService.java
@@ -3,6 +3,7 @@ package F12.newsfeedproject.domain.board.service;
 import F12.newsfeedproject.api.board.dto.BoardUpdateRequestDto;
 import F12.newsfeedproject.domain.board.entity.Board;
 import F12.newsfeedproject.domain.board.repository.BoardRepository;
+import F12.newsfeedproject.global.exception.board.BoardNotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class BoardService {
   }
 
   public Board findByBoardId(Long boardId) {
-    return boardRepository.findById(boardId).orElseThrow(IllegalArgumentException::new);
+    return boardRepository.findById(boardId).orElseThrow(BoardNotFoundException::new);
   }
 
   public List<Board> getBoards() {
@@ -28,13 +29,13 @@ public class BoardService {
 
   @Transactional
   public Board updateBoard(Long boardId, BoardUpdateRequestDto requestDto) {
-    Board board = boardRepository.findById(boardId).orElseThrow(IllegalArgumentException::new);
+    Board board = boardRepository.findById(boardId).orElseThrow(BoardNotFoundException::new);
     board.update(requestDto);
     return board;
   }
 
   public void deleteBoard(Long boardId) {
-    Board board = boardRepository.findById(boardId).orElseThrow(IllegalArgumentException::new);
+    Board board = boardRepository.findById(boardId).orElseThrow(BoardNotFoundException::new);
     boardRepository.deleteById(boardId);
   }
 }
