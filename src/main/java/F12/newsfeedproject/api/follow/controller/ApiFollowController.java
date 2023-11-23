11c9b91554f1ee9f3adafbd13f5779da7f371735
문정현 <foqlzm12345@gmail.com>
2023-11-23 17:37:33 +0900

    Feat : 권한 인증 추가 Refactor : Follow API ApiFollowService 중복 코드 메서드화 및 람다식 변환

--- src/main/java/F12/newsfeedproject/api/follow/controller/ApiFollowController.java
+++ src/main/java/F12/newsfeedproject/api/follow/controller/ApiFollowController.java
@@ -1,12 +1,15 @@
 package F12.newsfeedproject.api.follow.controller;
 
 import F12.newsfeedproject.api.follow.service.ApiFollowService;
+import F12.newsfeedproject.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,28 +18,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/follow")
 public class ApiFollowController {
 
-    private final ApiFollowService apiFollowService;
+  private final ApiFollowService apiFollowService;
 
-    @GetMapping("/{followerId}")
-    public ResponseEntity<?> followUser(
-        @PathVariable Long followerId) {
+  /**
+   * 팔로우 하기
+   * @param userDetails
+   * @param followerId
+   * @return
+   */
+  @PostMapping("/{followerId}")
+  public ResponseEntity<?> followUser(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
+      @PathVariable Long followerId) {
 
-        // security에서 꺼낸 유저 지금은 임시
-        String username = "test1";
+    String username = userDetails.getUsername();
 
-        apiFollowService.followUser(username, followerId);
-        return ResponseEntity.status(HttpStatus.OK).body("요청 성공");
-    }
+    apiFollowService.followUser(username, followerId);
+    return ResponseEntity.status(HttpStatus.OK).body("요청 성공");
+  }
 
-    @DeleteMapping("/{followerId}")
-    public ResponseEntity<?> unFollowUser(
-        @PathVariable Long followerId) {
+  /**
+   * 팔로우 취소
+   * @param followerId
+   * @return
+   */
+  @DeleteMapping("/{followerId}")
+  public ResponseEntity<?> unFollowUser(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
+      @PathVariable Long followerId) {
+    String username = userDetails.getUsername();
 
-        // security에서 꺼낸 유저 지금은 임시
-        String username = "test1";
-
-        apiFollowService.unFollowUser(username, followerId);
-        return ResponseEntity.status(HttpStatus.OK).body("요청 성공");
-    }
+    apiFollowService.unFollowUser(username, followerId);
+    return ResponseEntity.status(HttpStatus.OK).body("요청 성공");
+  }
 
 }
--- src/main/java/F12/newsfeedproject/api/follow/service/ApiFollowService.java
+++ src/main/java/F12/newsfeedproject/api/follow/service/ApiFollowService.java
@@ -1,7 +1,6 @@
 package F12.newsfeedproject.api.follow.service;
 
 import F12.newsfeedproject.domain.follow.entity.Follow;
-import F12.newsfeedproject.domain.follow.repository.FollowRepository;
 import F12.newsfeedproject.domain.follow.service.FollowService;
 import F12.newsfeedproject.domain.user.entity.User;
 import F12.newsfeedproject.domain.user.service.UserService;
@@ -18,56 +17,53 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ApiFollowService {
 
-    private final UserService userService;
-    private final FollowService followService;
+  private final UserService userService;
+  private final FollowService followService;
 
-    /**
-     * 팔로우 하기
-     * @param username
-     * @param followerId
-     */
-    public void followUser(String username, Long followerId) {
-        Optional<User> following = userService.findByUserName(username);
-        if (following.isEmpty()) {
-            throw new UserNotFoundException();
-        }
+  public void followUser(String username, Long followerId) {
+    User user = checkUser(username);
+    User follower = checkFollwer(followerId);
 
-        Optional<User> follower = userService.findByUserId(followerId);
-        if (follower.isEmpty()) {
-            throw new UserNotFoundException();
-        }
-
-        Optional<Follow> follow = followService.findByFollowingAndFollower(following.get(),
-            follower.get());
-        if (follow.isPresent()) {
-            throw new DuplicatedFollowException();
-        }
-        followService.saveFollow(new Follow(following.get(), follower.get()));
-
+    if (isAlreadyFollow(user, follower)) {
+      throw new DuplicatedFollowException();
     }
 
-    /**
-     * 언 팔로우 하기
-     * @param username
-     * @param followerId
-     */
-    public void unFollowUser(String username, Long followerId) {
-        Optional<User> following = userService.findByUserName(username);
-        if (following.isEmpty()) {
-            throw new UserNotFoundException();
-        }
+    followService.saveFollow(new Follow(user, follower));
+  }
 
-        Optional<User> follower = userService.findByUserId(followerId);
-        if (follower.isEmpty()) {
-            throw new UserNotFoundException();
-        }
+  public void unFollowUser(String username, Long followerId) {
+    User user = checkUser(username);
+    User follower = checkFollwer(followerId);
 
-        Optional<Follow> follow = followService.findByFollowingAndFollower(following.get(), follower.get());
-        if (follow.isEmpty()) {
-            throw new FollowNotFoundException();
-        }
-        followService.deleteFollow(follow.get());
+    if (!isAlreadyFollow(user, follower)) {
+      throw new FollowNotFoundException();
     }
+    Optional<Follow> follow = followService.findByFollowingAndFollower(user, follower);
+
+    followService.deleteFollow(follow.get());
+  }
+
+  private User checkUser(String username) {
+    Optional<User> user = userService.findByUserName(username);
+    if (user.isEmpty()) {
+      throw new UserNotFoundException();
+    }
+    return user.get();
+  }
+
+  private User checkFollwer(Long followerId) {
+    Optional<User> follower = userService.findByUserId(followerId);
+    if (follower.isEmpty()) {
+      throw new UserNotFoundException();
+    }
+    return follower.get();
+  }
+
+  private boolean isAlreadyFollow(User following, User follower) {
+    Optional<Follow> optionalFollow = followService.findByFollowingAndFollower(following,
+        follower);
+    return optionalFollow.isPresent();
+  }
 
 }
 
